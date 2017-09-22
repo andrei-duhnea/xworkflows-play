7149edba60933de5820b6474c326fb8fd5619db9
Andrei Duhnea <andrei@duhnea.net>
2017-09-22 14:21:29 +0300

    Configurable check expressions

--- demo.py
+++ demo.py
@@ -1,11 +1,33 @@
 """
 >>> log.debug = nolog
->>> rep = Report(title='Report1')
+>>> log.error = nolog
+>>> checks_list = {
+...    'has_content': (
+...        'doc.content is not None and len(doc.content) > 0',
+...        'Content is empty'
+...    ),
+...    'title_size': (
+...        'len(doc.title) >= 8',
+...        'Title too short (at least 8 chars)'
+...    )
+... }
+...
+>>> rep = Report(title='Report1', checks=checks_list)
 >>> rep.state.name
 'draft'
 >>> rep.prepare('Larry')
 >>> rep.state.title
 'Ready'
+>>> rep.finish('Curly')
+Traceback (most recent call last):
+...
+Exception: Content is empty
+>>> rep.content = 'foo'
+>>> rep.finish('Curly')
+Traceback (most recent call last):
+...
+Exception: Title too short (at least 8 chars)
+>>> rep.title = 'Report_1'
 >>> rep.finish('Curly')
 >>> rep.history
 ['Larry: prepare', 'Curly: finish']
@@ -21,6 +43,7 @@ xworkflows.base.InvalidTransitionError: Transition 'finish' isn't available from
 >>> rep.history
 ['Larry: prepare', 'Curly: finish', 'Curly: submit', 'Moe: reject', 'Larry: finish', 'Larry: submit', 'Moe: approve']
 """
+
 from functools import wraps
 
 import logging
@@ -84,16 +107,81 @@ def keep_history(f):
             user = kwargs['user'] = 'UNKNOWN'
         instance.history.append(f'{user}: {f.__name__}')
         return f(instance, *args, **kwargs)
+
     return wrapper
+
+
+def check_all(*checks):
+    """Transition decorator, raises an exception if any of the checks fails."""
+
+    def _check_all(f):
+        @wraps(f)
+        def wrapper(instance, *args, **kwargs):
+            for check in checks:
+                cond = instance.checks[check]
+                if not cond.verify({'doc': instance}):
+                    log.error('Check failed', name=check, error=cond.error_msg)
+                    raise Exception(cond.error_msg)
+            return f(instance, *args, **kwargs)
+
+        return wrapper
+
+    return _check_all
+
+
+def check_any(*checks):
+    """Transition decorator, raises an exception if all of the checks fail."""
+
+    def _check_all(f):
+        @wraps(f)
+        def wrapper(instance, *args, **kwargs):
+            failures = []
+            for check in checks:
+                cond = instance.checks[check]
+                if not cond.verify({'doc': instance}):
+                    failures.append((check, cond.error_msg))
+            if failures:
+                log.error('All checks failed', transition=f.__name__, checks=checks)
+                raise Exception(f'All checks failed for {f.__name__}')
+            else:
+                return f(instance, *args, **kwargs)
+
+        return wrapper
+
+    return _check_all
+
+
+class Check:
+    def __init__(self, expr, error_msg=None):
+        self.expr = expr
+        self._expr_obj = None
+        self.error_msg = error_msg
+
+    def compile(self):
+        return compile(self.expr, '<string>', 'eval')
+
+    @property
+    def expr_obj(self):
+        """Caches the compiled expression"""
+        if self._expr_obj is None:
+            self._expr_obj = compile(self.expr, '<string>', 'eval')
+        return self._expr_obj
+
+    def verify(self, *args):
+        """Evaluates the expression.
+        Will pass locals and globals to `eval`"""
+        return eval(self.expr_obj, *args)
 
 
 class Report(WorkflowEnabled):
     state = ReportWorkflow()
 
-    def __init__(self, title, content=None):
+    def __init__(self, title, content=None, checks=None):
         self.title = title
         self.content = content
         self.history = []
+        checks = checks or {}
+        self.checks = {name: Check(*spec) for name, spec in checks.items()}
 
     @transition()
     @keep_history
@@ -101,6 +189,7 @@ class Report(WorkflowEnabled):
         pass
 
     @transition()
+    @check_all('has_content', 'title_size')
     @keep_history
     def finish(self, user):
         pass
@@ -140,4 +229,5 @@ class Report(WorkflowEnabled):
 
 if __name__ == '__main__':
     import doctest
+
     doctest.testmod()
